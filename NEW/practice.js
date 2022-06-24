00ce2d6f77a436ab0140adb701c7c1f8c92f602a
shubhaml0099 <shubhamlangote0099@gmail.com>
2022-06-24 20:53:25 +0530

    firstCommit

--- NEW/practice.js
+++ NEW/practice.js
@@ -9,3 +9,28 @@ function sum(x,y){
 
 console.log(ss)
 
+// function expression 
+
+let sum1 = function(){
+    return x+y
+}
+
+// function decliration 
+
+function sum2(){
+    return x+y
+}
+
+// what is call back finction 
+// ==> one function pass as argument or as parametar to another function call as cal back function 
+// ==>> why call back function require ==>> 
+
+
+function add2(){
+    console.log('hello')
+}
+function add(x,y,fn){
+    console.log(x+y)
+    fn()
+}
+add(2,5,add2)
